6fdf69cbd300aa53e2e3d19bb53315a5249e82e4
Дід Alik <didalik.aim@gmail.com>
2024-01-14 22:40:34 +0000

    Add function start_local_dev

--- qa/handle_request.sh
+++ qa/handle_request.sh
@@ -6,41 +6,23 @@
 # LICENSE file in the root directory of this source tree.
 ##
 
-start_local_dev () { # {{{1
-  local svc_name=$1
-  local svc_count=$2
-  local svc_dir=../../../svc/${svc_name}
-  local dev_fifo=${svc_dir}/dev.fifo
-  local dev_script=${svc_dir}/${svc_name}-dev.sh
-  local grep_pattern='wrangler-dist/cli.js dev'
-
-  echo "- $0 checking local svc ${svc_name}..." >> error.log
-  if [ $(ps -ef|grep "${svc_name}-dev.sh"|wc -l) -eq 1 ]; then
-    echo "- $0 starting local svc ${svc_name}..." >> error.log
-    $dev_script >> error.log &
-    echo $svc_name > $dev_fifo
-    while [ $(ps -ef|grep "$grep_pattern"|wc -l) -lt $svc_count ]; do
-      echo -e "\t- $dev_script starting wrangler dev..." >> error.log
-      sleep 1
-    done
-    sleep 1
-  fi
-  echo "- $0 local svc ${svc_name} is ON." >> error.log
-}
+LOCALDEV_LOG=error.log # {{{1
 
 teardown () { # {{{1
+  while read;do kill $REPLY;done <.pids2kill
   kill $PID_OF_TAIL
-  echo "- teardown killed PID_OF_TAIL $PID_OF_TAIL" >> error.log
+  echo "- teardown killed dev pids and PID_OF_TAIL $PID_OF_TAIL" >> $LOCALDEV_LOG
 }
 trap teardown EXIT
 
-rm -f loop.log error.log # {{{1 
+rm -f .pids2kill loop.log $LOCALDEV_LOG # {{{1 
 touch loop.log
 tail -n 999999 -f loop.log &
 PID_OF_TAIL=$!
 
 . ../config.env
 . config.env
+. ../dak/util/public/lib/util.sh
 [ $LOCAL_DEV_REQUESTED ] && start_local_dev index 2 && start_local_dev hex 3
 
-$RUN_MJS handle_request >> loop.log 2>>error.log # {{{1
+$RUN_MJS handle_request >> loop.log 2>>$LOCALDEV_LOG # {{{1
